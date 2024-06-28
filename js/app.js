bcaad12db2900aaab7551c0e540582e23a4cc33a
kyleabrahams <47188480+kyleabrahams@users.noreply.github.com>
2024-06-28 08:14:49 -0400

    first commit

--- js/app.js
+++ js/app.js
@@ -17,3 +17,65 @@ $(document).ready(function(){
 			return false;
 		});
 });
+
+// Counter animation
+
+function isOnScreen(elem) {
+	// if the element doesn't exist, abort
+	if( elem.length == 0 ) {
+		return;
+	}
+	var $window = jQuery(window)
+	var viewport_top = $window.scrollTop()
+	var viewport_height = $window.height()
+	var viewport_bottom = viewport_top + viewport_height
+	var $elem = jQuery(elem)
+	var top = $elem.offset().top
+	var height = $elem.height()
+	var bottom = top + height
+
+	return (top >= viewport_top && top < viewport_bottom) ||
+	(bottom > viewport_top && bottom <= viewport_bottom) ||
+	(height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
+}
+
+jQuery( document ).ready( function() {
+	window.addEventListener('scroll', function(e) {
+		// if( isOnScreen( jQuery( '.counter-count' ) ) ) { /* Pass element id/class you want to check */
+		if( isOnScreen( jQuery( '.counter-count') ) ) { /* Pass element id/class you want to check */
+		
+		// $('.counter-count').each(function () {
+		// $('.counter-count').on("scroll", function () {
+
+		// 	$(this).prop('Counter',0).animate({
+		// 		Counter: $(this).text()
+		// 	}, {
+		// 		duration: 2500,
+		// 		easing: 'swing',
+		// 		step: function (now) {
+		// 			$(this).text(Math.ceil(now));
+		
+			
+			
+			
+			
+			
+			// alert( 'The specified container is in view.' );
+ 		}	
+	});
+	
+});
+
+
+
+// $('.counter-count').each(function () {
+// 	$(this).prop('Counter',0).animate({
+// 		Counter: $(this).text()
+// 	}, {
+// 		duration: 2500,
+// 		easing: 'swing',
+// 		step: function (now) {
+// 			$(this).text(Math.ceil(now));
+// 		}
+// 	});
+// });
